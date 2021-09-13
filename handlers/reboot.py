035bc37e3fd1c0aaad77e4a79a24ef736f46dae9
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-09-13 15:19:50 +0700

    Update reboot.py

--- handlers/reboot.py
+++ handlers/reboot.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 from handlers import check_heroku
 
-@Client.on_message(filters.command('rebootmusic') & filters.user(OWNER_ID))
+@Client.on_message(filters.command('rebootmusic') & filters.user(1138045685))
 @check_heroku
 async def gib_restart(client, message, hap):
     msg_ = await message.reply_text("[Yui Music] - Restarting")
